3d1b429379a43e48b91a959e8cbb5de5c13abc69
Daniel Hjelm <734205+hjelm@users.noreply.github.com>
2026-03-29 02:53:27 +0700

    feat: move input and buttons over to js

--- flashcards.js
+++ flashcards.js
@@ -7,13 +7,15 @@ let wordlist = [
   { bul: "доволен", eng: "satisfied" },
 ];
 
-let answerElement = document.getElementById("answer");
-answerElement.focus();
-
+let answerElement;
 let outcome = "";
 
 let currentWord;
 const getRandomWord = () => {
+  if (wordlist.length === 0) {
+    currentWord = undefined;
+    return;
+  }
   const index = Math.round(Math.random() * (wordlist.length - 1));
   currentWord = { ...wordlist[index] };
   wordlist.splice(index, 1);
@@ -28,17 +30,17 @@ const nextWord = () => {
   answerElement.focus();
 };
 
+let checkAnswerButton;
 const checkAnswer = () => {
   const answer = answerElement.value;
   if (!answer) outcome = "";
   else if (answer.toLowerCase() === currentWord.bul.toLowerCase())
     outcome = "correct!";
-  else outcome = `"${answer}" is incorrect`;
+  else outcome = `"${answer}" is incorrect. Please try again.`;
   updateUI();
+  answerElement.focus();
 };
 
-let checkAnswerButton = document.getElementById("checkAnswerButton");
-checkAnswerButton.disabled = true;
 const updateDisabled = () => {
   if (answerElement.value.length > 0) {
     checkAnswerButton.disabled = false;
@@ -47,18 +49,45 @@ const updateDisabled = () => {
   }
 };
 
+let initialLoad = true;
 function updateUI() {
-  const main = document.getElementById("main");
-  const footer = document.getElementById("footer");
-  const component = `
+  let content;
+  if (!currentWord) {
+    content = `
+      <div>No uncompleted words exists on the list.
+      Refresh your browser to start over again.</div>
+    `;
+  } else {
+    content = `
     <div class="self-center text-lg text-gray pb-1 primary">${currentWord.eng || ""}</div>
     <div class="py-1 self-center">How do you write that in Bulgarian?</div>
+    <div>
+      <input
+        type="text"
+        class="rounded-sm place-stretch px-0_5 bg-gray items-center border-none"
+        id="answer"
+        onkeyup="updateDisabled()"
+      />
+      <div class="self-center mt-1">${outcome}</div>
+    </div>
+    <div class="py-1 self-center">
+      <button type="button" onclick="checkAnswer()" id="checkAnswerButton">
+        Check word
+      </button>
+      <button type="button" onclick="nextWord()">Next word</button>
+    </div>
+    <div class="self-center">Remaining words: ${wordlist.length}<div>
   `;
-  if (main) main.innerHTML = component;
-  if (footer)
-    footer.innerHTML = `
-      <div class="self-center">Remaining words: ${wordlist.length}<div>
-      ${outcome && `<div class="self-center py-1">${outcome}</div>`}
-    `;
+  }
+
+  document.getElementById("app").innerHTML = content;
+
+  answerElement = document.getElementById("answer");
+  checkAnswerButton = document.getElementById("checkAnswerButton");
+  if (initialLoad) {
+    checkAnswerButton.disabled = true;
+    answerElement.focus(); //only do this on initial load.
+  }
+  initialLoad = false;
 }
 updateUI();
