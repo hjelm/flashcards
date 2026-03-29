2223d4a04f909233221981adce25c6d37f463cdc
Daniel Hjelm <734205+hjelm@users.noreply.github.com>
2026-03-29 10:19:16 +0700

    feat: remove check word button; remove global vars

--- flashcards.js
+++ flashcards.js
@@ -1,117 +1,110 @@
-const wordlist = [
-  { bul: "дразнещ", eng: "irritating" },
-  { bul: "тъкмо", eng: "just now" },
-  { bul: "поздравления", eng: "congratulations" },
-  { bul: "споделяне", eng: "share" },
-  { bul: "скучно", eng: "bored" },
-  { bul: "доволен", eng: "satisfied" },
-];
+(() => {
+  const wordList = [
+    { bul: "дразнещ", eng: "irritating" },
+    { bul: "тъкмо", eng: "just now" },
+    { bul: "поздравления", eng: "congratulations" },
+    { bul: "споделяне", eng: "share" },
+    { bul: "скучно", eng: "bored" },
+    { bul: "доволен", eng: "satisfied" },
+  ];
+  const total = wordList.length;
+  let score = 0;
+  const mistakeList = [];
 
-const errorList = [];
+  let answerElement;
+  let outcome = "";
 
-const total = wordlist.length;
-let score = 0;
-
-let answerElement;
-let outcome = "";
-
-let currentWord;
-const getRandomWord = () => {
-  if (wordlist.length === 0) {
-    currentWord = undefined;
-    return;
-  }
-  const index = Math.round(Math.random() * (wordlist.length - 1));
-  currentWord = { ...wordlist[index] };
-  wordlist.splice(index, 1);
-};
-getRandomWord();
-
-const nextWord = () => {
-  answerElement.value = "";
-  outcome = "";
+  let currentWord;
+  const getRandomWord = () => {
+    if (wordList.length === 0) {
+      currentWord = undefined;
+      return;
+    }
+    const index = Math.round(Math.random() * (wordList.length - 1));
+    currentWord = { ...wordList[index] };
+    wordList.splice(index, 1);
+  };
   getRandomWord();
-  updateUI();
-  if (currentWord) answerElement.focus();
-};
 
-let checkAnswerButton;
-const checkAnswer = () => {
-  const answer = answerElement.value;
-  if (!answer) outcome = "";
-  else if (answer.toLowerCase() === currentWord.bul.toLowerCase()) {
-    outcome = "correct!";
-    score = score + 1;
-  } else {
-    outcome = `"${answer}" is incorrect.`;
-    errorList.push({ ...currentWord, answer: answer });
-  }
-  setTimeout(() => nextWord(), 700);
-  updateUI();
-  answerElement.value = answer;
-  answerElement.focus();
-};
+  const nextWord = () => {
+    answerElement.value = "";
+    outcome = "";
+    getRandomWord();
+    updateUI();
+    if (currentWord) answerElement.focus();
+  };
 
-const updateDisabled = () => {
-  if (answerElement.value.length > 0) {
-    checkAnswerButton.disabled = false;
-  } else {
-    checkAnswerButton.disabled = true;
-  }
-};
+  let checkAnswerButton;
+  const checkAnswer = () => {
+    const answer = answerElement.value;
+    if (!answer) outcome = "";
+    else if (answer.toLowerCase() === currentWord.bul.toLowerCase()) {
+      outcome = "correct!";
+      score = score + 1;
+    } else {
+      outcome = `"${answer}" is incorrect.`;
+      mistakeList.push({ ...currentWord, answer: answer });
+    }
+    setTimeout(() => nextWord(), 700);
+    updateUI();
+    answerElement.value = answer;
+    answerElement.focus();
+  };
 
-let initialLoad = true;
-function updateUI() {
-  let content;
-  if (!currentWord) {
-    content = `
+  let initialLoad = true;
+  function updateUI() {
+    let content;
+    if (!currentWord) {
+      content = `
       <p>No uncompleted words exists on the list.
       Refresh your browser to start over again.</p>
-    `;
-    if (errorList)
-      content += `
+      `;
+      if (mistakeList)
+        content += `
         <div class="list-title">Errors:</div>
         <ul>
-        ${errorList.map((e) => `<li>"${e.answer}" correct: "${e.bul}"</li>`).join("\n")}
+        ${mistakeList.map((e) => `<li>"${e.answer}" correct: "${e.bul}"</li>`).join("\n")}
         <ul>
-      `;
-  } else {
-    content = `
+        `;
+    } else {
+      content = `
       <div class="self-center text-lg text-gray pb-1 primary">
         ${currentWord.eng || ""}
       </div>
       <div class="py-1 self-center">How do you write that in Bulgarian?</div>
+      <form id="answerForm">
       <div>
         <input
           type="text"
           class="rounded-sm place-stretch px-0_5 bg-gray items-center border-none"
           id="answer"
-          onkeyup="updateDisabled()"
         />
         <div class="self-center mt-1">${outcome}</div>
       </div>
-      <div class="py-1 self-center">
-        <button type="button" onclick="checkAnswer()" id="checkAnswerButton">
-          Check word
-        </button>
-      </div>
+      </form>
       <div class="self-center">
-        Remaining words: ${wordlist.length}
+        Remaining words: ${wordList.length}
       </div>
       <div class="self-center py-1">
         Score: ${score}/${total}
       </div>
     `;
-  }
+    }
 
-  document.getElementById("app").innerHTML = content;
+    document.getElementById("app").innerHTML = content;
 
-  answerElement = document.getElementById("answer");
-  checkAnswerButton = document.getElementById("checkAnswerButton");
-  if (initialLoad) {
-    checkAnswerButton.disabled = true;
-    answerElement.focus(); //only do this on initial load.
+    answerElement = document.getElementById("answer");
+    const answerForm = document.getElementById("answerForm");
+    if (answerForm) {
+      answerForm.onsubmit = (e) => {
+        e.preventDefault();
+        checkAnswer();
+      };
+    }
+    if (initialLoad) {
+      answerElement.focus();
+    }
+    initialLoad = false;
   }
-  initialLoad = false;
-}
-updateUI();
+  updateUI();
+})();
