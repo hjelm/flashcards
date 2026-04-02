f930cbf3426fff9d6b805c5b9486ae9975d8960a
Daniel Hjelm <734205+hjelm@users.noreply.github.com>
2026-04-02 13:46:37 +0700

    feat: add html tagged template literal; initial answer diff

--- flashcards.js
+++ flashcards.js
@@ -48,7 +48,7 @@ const checkAnswer = () => {
     answerInputBgColor = "green";
     score = score + 1;
   } else {
-    outcome = `"${answer}" is incorrect.`;
+    outcome = `"${diffAnswer(answer, currentWord.bul)}" is incorrect.`;
     answerInputBgColor = "red";
     mistakeList.push({ ...currentWord, answer: answer });
   }
@@ -62,6 +62,19 @@ const checkAnswer = () => {
   answerInput.focus();
 };
 
+const diffAnswer = (answer, target) => {
+  let ok = "";
+  let bad = "";
+  for (i in answer) {
+    if (answer[i] === target[i]) ok += answer[i];
+    else {
+      bad = answer.substring(i + 1);
+      break;
+    }
+  }
+  return `<span class="ok">${ok}</span><span class="bad">${bad}</span>`;
+};
+
 navigation.onnavigate = (e) => {
   hash = new URLPattern(e.destination.url).hash;
   updateUI();
@@ -70,63 +83,72 @@ location.onnavigate = (e) => {
   console.log(e);
 };
 
+function html(strings, ...values) {
+  const template = document.createElement("template");
+  template.innerHTML = String.raw(strings, ...values);
+  return template.content;
+}
+
 function updateUI() {
   let content;
   document.getElementById("listLink").style.textDecoration = "unset";
   document.getElementById("examLink").style.textDecoration = "underline";
   if (hash === "list") {
-    content = `
+    content = html`
       <table style="font-size: 2rem;">
         <thead>
-        <td style="border-bottom: solid">Bulgarian</td>
-        <td style="border-bottom: solid">English</td>
+          <td style="border-bottom: solid">Bulgarian</td>
+          <td style="border-bottom: solid">English</td>
         </thead>
         <tbody>
-        ${collection[selectedList].list.map((w) => `<tr><td>${w.bul}</td><td>${w.eng}</td></tr>`).join("\n")}
+          ${collection[selectedList].list
+            .map((w) => `<tr><td>${w.bul}</td><td>${w.eng}</td></tr>`)
+            .join("\n")}
         </tbody>
       </table>
     `;
     document.getElementById("listLink").style.textDecoration = "underline";
     document.getElementById("examLink").style.textDecoration = "unset";
   } else if (!currentWord) {
-    content = `
-      <p>No uncompleted words exists on the current list.<br/>
-      Refresh your browser to start over again.</p>
-      `;
+    content = html` <p>All words have been covered.</p> `;
     if (mistakeList.length > 0)
-      content += `
+      content.append(html`
         <div class="list-title">Your mistakes:</div>
         <ul>
-        ${mistakeList.map((m) => `<li>For ${m.eng} you wrote "${m.answer}", correct is "${m.bul}"</li>`).join("\n")}
-        <ul>
-        `;
+          ${mistakeList
+            .map(
+              (m) => `
+          <li><span class="text-xl">${m.eng}:</span> you wrote<br>
+          <span class="text-xl">"${diffAnswer(m.answer, m.bul)}"</span>, correct is<br>
+          <span class="text-xl">"${m.bul}"</span></li>`,
+            )
+            .join("\n")}
+        </ul>
+        <p>Refresh your browser to start over again.</p>
+      `);
   } else {
-    content = `
+    content = html`
       <div class="self-center text-lg text-gray pb-1 primary">
         ${currentWord.eng || ""}
       </div>
       <div class="py-1 self-center">How do you write that in Bulgarian?</div>
       <form id="answerForm">
-      <div>
-        <input
-          type="text"
-          placeholder="Enter answer"
-          class="rounded-sm place-stretch px-0_5 items-center border-none"
-          style="background-color: ${answerInputBgColor}"
-          id="answerInput"
-        />
-        <div class="self-center mt-1">${outcome}</div>
-      </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter answer"
+            class="rounded-sm place-stretch px-0_5 items-center border-none"
+            style="background-color: ${answerInputBgColor}"
+            id="answerInput"
+          />
+          <div class="self-center mt-1">${outcome}</div>
+        </div>
       </form>
-      <div class="self-center">
-        Remaining words: ${wordList.length}
-      </div>
-      <div class="self-center py-1">
-        Score: ${score}/${total}
-      </div>
+      <div class="self-center">Remaining words: ${wordList.length}</div>
+      <div class="self-center py-1">Score: ${score}/${total}</div>
     `;
   }
-  document.getElementById("app").innerHTML = content;
+  document.getElementById("app").replaceChildren(content);
 
   const answerForm = document.getElementById("answerForm");
   if (answerForm) {
